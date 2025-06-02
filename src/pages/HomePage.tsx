3bdfa2312fa8d98e7eb8fd2aa3ace73118a91991
Roman Punko <romanpunko2004@gmail.com>
2025-06-03 01:12:41 +0300

    fix: tailwind config colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/header/Header.js';
 
-
-const HomePage = () =>{
-
-  return(
-    111
-  )
-}
+const HomePage = () => {
+  return (
+    <div className='bg-primary h-screen text-text'>
+      <Header />
+    </div>
+  );
+};
 
 export default HomePage;
