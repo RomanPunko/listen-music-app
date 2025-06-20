4722c886ba33e4660db325503d3be9d8e0baff51
Roman Punko <romanpunko2004@gmail.com>
2025-06-20 14:08:52 +0300

    feat: add to store playlists and songs slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth-slice';
-import playlistReducer from './slices/playlist-slice'
+import playlistReducer from './slices/playlists-data-slice';
+import songReducer from './slices/songs-data-slice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   playlist: playlistReducer,
+  song: songReducer,
 });
 
 export const setupStore = () => {
