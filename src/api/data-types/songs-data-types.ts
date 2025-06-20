05d5067adb899f681b3f3f69ada8a2988d4055a3
Roman Punko <romanpunko2004@gmail.com>
2025-06-20 14:16:43 +0300

    feat: songs data types

--- src/api/data-types/songs-data-types.ts
+++ src/api/data-types/songs-data-types.ts
@@ -2,7 +2,11 @@ export interface ISong {
   id: string;
   name: string;
   artist: string;
+  genre: string;
+  listens: string;
   avatar: string;
   time: string;
   urlSong: string;
 }
+
+export type IAllSongs = ISong[];
