5b92c5423a75f3c30c2a7583a1b3011fa81bfe00
Roman Punko <romanpunko2004@gmail.com>
2025-06-30 15:23:03 +0300

    feat: playerSongInfo in audio player

--- src/components/audio-player/PlayerSongInfo.tsx
+++ src/components/audio-player/PlayerSongInfo.tsx
@@ -1,20 +1,21 @@
 import { type FC } from 'react';
+import { useAppSelector } from '@/hooks/app-hooks';
 
 const PlayerSongInfo: FC = () => {
+  const currentSong = useAppSelector((state) => state.audio.currentSong);
+
   return (
-    <>
-    </>
-    // <div className="flex items-center">
-    //   {currentSong && (
-    //     <>
-    //       <img src={currentSong.avatar} alt={currentSong.name} className="w-10 h-10 rounded-md" />
-    //       <div className="ml-5">
-    //         <div className="">{currentSong.name}</div>
-    //         <div className="">{currentSong.artist}</div>
-    //       </div>
-    //     </>
-    //   )}
-    // </div>
+    <div className="flex items-center absolute top-1/2 left-4 -translate-y-1/2">
+      {currentSong && (
+        <>
+          <img src={currentSong.avatar} alt={currentSong.name} className="w-10 h-10 rounded-md" />
+          <div className="ml-5">
+            <div className="">{currentSong.name}</div>
+            <div className="">{currentSong.artist}</div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
