323914b654eb9facd3bbd7924e019533475f3b31
Roman Punko <romanpunko2004@gmail.com>
2025-07-20 13:56:58 +0300

    removed: the play button from playlistCard

--- src/components/playlists/PlaylistCard.tsx
+++ src/components/playlists/PlaylistCard.tsx
@@ -1,7 +1,6 @@
 import { type FC } from 'react';
 import { type IPlaylist } from '@/api/data-types/playlist-data-types';
 import { useNavigate } from 'react-router-dom';
-import { IoIosPlay } from 'react-icons/io';
 
 interface IPlaylistCardProps {
   playlist: IPlaylist;
@@ -26,10 +25,6 @@ const PlaylistCard: FC<IPlaylistCardProps> = ({ playlist }) => {
       />
       <p className="font-bold">{playlist.artist}</p>
       <p className="opacity-60 text-[14px]">{playlist.name}</p>
-      <IoIosPlay
-        className="absolute top-37 right-4 bg-green-600 rounded-full w-12 h-12 p-3 text-white
-                   cursor-pointer hover:bg-green-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-      />
     </div>
   );
 };
